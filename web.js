05907528c05211e40cc505f3f526bfadd5014373
Matt Hall <matthew.b.hall@gmail.com>
2013-07-10 05:52:16 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var res = fs.readFileSync('index.html').toString('utf8');
+  response.send(res);
 });
 
 var port = process.env.PORT || 5000;
